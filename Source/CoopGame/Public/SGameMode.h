43d4273b831cc5561734ad1d24c8d87718724a7c
Maurel Sagbo <sagbomaurel@yahoo.fr>
2020-11-02 18:38:25 +0100

    Section 13: Complete Waves logic

--- Source/CoopGame/Public/SGameMode.h
+++ Source/CoopGame/Public/SGameMode.h
@@ -13,5 +13,42 @@ UCLASS()
 class COOPGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+protected:
+	FTimerHandle TimerHandle_BotSpawner;
+
+	FTimerHandle TimerHandle_NextWaveStart;
+
+	int32 NrOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
 	
+protected:
+	// Hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	// Start spawning bots
+	void StartWave();
+
+	// Stop spawning bots
+	void EndWave();
+
+	// Set timer for next startwave
+	void PrepareForNextWave();
+
+	void CheckWaveState();
+
+public:
+	ASGameMode();
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void StartPlay() override;
+
 };
